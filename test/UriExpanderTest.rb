16caa76314f1c6b3206ae3b2ede0d7834a58fb09
Lukas Jäger <lukas_jaeger@gmx.net>
2021-09-15 14:58:25 +0200

    Refactor UriExpanderTest

--- test/UriExpanderTest.rb
+++ test/UriExpanderTest.rb
@@ -18,28 +18,32 @@ require_relative "../src/UriExpander"
 
 class UriExpanderTest < Test::Unit::TestCase
   def test_expand_uri_if_necessary_global_uri()
-    uri_to_expand = "https://crimsonsunrise.hopto.org/test/img/flag_english.svg"
-    domain_uri = "https://crimsonsunrise.hopto.org/test" 
-    expander = UriExpander.new()
-    expanded_uri = expander.expand_uri_if_necessary(uri_to_expand, domain_uri)
-    assert_equal(uri_to_expand, expanded_uri)
+    assert_true(generic_test(
+      "https://crimsonsunrise.hopto.org/test/img/flag_english.svg",
+      "https://crimsonsunrise.hopto.org/test",
+      "https://crimsonsunrise.hopto.org/test/img/flag_english.svg"
+    ))
   end
   
   def test_expand_uri_if_necessary_local_uri()
-    uri_to_expand = "img/flag_english.svg"
-    domain_uri = "https://crimsonsunrise.hopto.org/test" 
-    expander = UriExpander.new()
-    expanded_uri = expander.expand_uri_if_necessary(uri_to_expand, domain_uri)
-    expected_uri = "https://crimsonsunrise.hopto.org/test/img/flag_english.svg"
-    assert_equal(expected_uri, expanded_uri)
+    assert_true(generic_test(
+      "img/flag_english.svg",
+      "https://crimsonsunrise.hopto.org/test",
+      "https://crimsonsunrise.hopto.org/test/img/flag_english.svg"
+    ))
   end
   
   def test_expand_uri_if_necessary_local_uri_with_trailing_slash()
-    uri_to_expand = "img/flag_english.svg"
-    domain_uri = "https://crimsonsunrise.hopto.org/test/" 
+    assert_true(generic_test(
+      "img/flag_english.svg",
+      "https://crimsonsunrise.hopto.org/test/",
+      "https://crimsonsunrise.hopto.org/test/img/flag_english.svg"
+    ))
+  end
+
+  def generic_test(uri_to_expand, domain_uri, expected_uri)
     expander = UriExpander.new()
     expanded_uri = expander.expand_uri_if_necessary(uri_to_expand, domain_uri)
-    expected_uri = "https://crimsonsunrise.hopto.org/test/img/flag_english.svg"
-    assert_equal(expected_uri, expanded_uri)
+    return expected_uri == expanded_uri
   end
 end
